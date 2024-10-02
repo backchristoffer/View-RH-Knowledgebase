e8a33213749c6da6d6bf79773213e9be8c863924
backchristoffer <backchristoffer@hotmail.com>
2024-10-02 10:12:51 +0200

    use the cli to search kcs or any other keyword. will return ALL so parsing might be needed here. this is just a demo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import argparse
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -21,3 +22,46 @@ def get_access_token():
     except Exception as e:
         print(f"Error getting access token: {e}")
         return None
+    
+#based on https://access.redhat.com/management/api/case_management#/search/getKcsSearchResults
+def get_kcs_articles(keyword, limit=5):
+    access_token = get_access_token()
+
+    if not access_token:
+        raise Exception("Failed to get access token")
+    
+    kcs_api_url = "https://api.access.redhat.com/support/search/kcs"
+    headers = {
+        'Authorization': f"Bearer {access_token}",
+        'Accept': 'application/json'
+    }
+    params = {
+        'q': keyword,
+        'rows': limit
+    }
+
+    response = requests.get(kcs_api_url, headers=headers, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"failed to get the response. status code: {response.status_code}")
+
+#need to be able to use this as CLI at first. WIP
+def main():
+    parser = argparse.ArgumentParser(
+        description='fetch KCS articles',
+        epilog='Example: python3 app.py 6988487 --limit 1'
+    )
+    parser.add_argument('query', type=str, help='Search query string (e.g., document id or keyword)')
+    parser.add_argument('--limit', type=int, default=5, help='Limit number of results (default: 5)')
+
+    args = parser.parse_args()
+
+    data = get_kcs_articles(args.query, limit=args.limit)
+
+    if data:
+        import json
+        print(json.dumps(data, indent=4))
+
+if __name__ == '__main__':
+    main()
